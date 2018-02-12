9745d06b3c398466872803026cb36f614783ed47
PJ Frias <PeejFrias@Gmail.com>
2018-02-12 13:21:50 -0500

    add flash messages for user signup

--- app/controllers/prayer_controller.rb
+++ app/controllers/prayer_controller.rb
@@ -1,7 +1,16 @@
 class PrayerController < ApplicationController
 
-	get 'prayers' do 
+	get '/prayers' do 
+		@prayers = Prayer.all
 		erb :"prayers/index"
+	end
+
+	get '/prayers/new' do
+		erb :"prayers/new"
+	end
+
+	post '/prayers' do
+		raise params.inspect
 	end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,6 +12,7 @@ class UserController < ApplicationController
 		user = User.new(params)
 		if user.save
 			session[:user_id] = user.id
+			flash.next[:greeting] = "Hi there, #{user.username}"
 			redirect '/prayers'
 		else
 			flash.next[:message] = "Username, email, and password are required to create an account."
