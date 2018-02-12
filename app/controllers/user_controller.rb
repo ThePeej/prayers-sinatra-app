d3be3409af16a6ea951f9948d93b24b1fa4591ab
PJ Frias <PeejFrias@Gmail.com>
2018-02-12 14:00:31 -0500

    add login and show page for user account info

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,9 +15,49 @@ class UserController < ApplicationController
 			flash.next[:greeting] = "Hi there, #{user.username}"
 			redirect '/prayers'
 		else
-			flash.next[:message] = "Username, email, and password are required to create an account."
+			flash.next[:error] = "Username, email, and password are required to create an account."
 			redirect '/signup'
 		end
+	end
+
+	get '/login' do
+		if logged_in?
+			redirect '/prayers'
+		else
+			erb :"users/login"
+		end
+	end
+
+	post '/login' do
+		user = User.find_by(:username => params["username"])
+
+	    if user && user.authenticate(params["password"])
+	        session[:user_id] = user.id
+	        flash.next[:greeting] = "Welcome back, #{user.username}"
+	        redirect "/prayers"
+	    else
+	    	flash.next[:error] = "Login Failed. Please Try Again."
+	        redirect "/login"
+	 	end
+ 	 end
+
+	get '/account' do
+		if logged_in?
+			@user = current_user
+			erb :"users/show"
+		else
+			redirect '/login'
+		end
+	end
+
+	get '/users/:id/edit' do
+		@user = User.find(params[:id])
+	end
+
+	get '/logout' do
+		session.clear
+		flash.next[:greeting] = "You've been logged out"
+		redirect '/prayers'
 	end
 
 end
