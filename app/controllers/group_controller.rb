f58ad5da360720c150cb7db0c10e2f7c7bbb5042
PJ Frias <PeejFrias@Gmail.com>
2018-02-12 15:57:09 -0500

    add group index page

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,7 +1,35 @@
 class GroupController <ApplicationController
 
 	get '/groups' do 
-		@groups = Group.find_by(:private => false)
-
+		@groups = Group.all
 		erb :"groups/index"
 	end
+
+	get '/groups/new' do
+		erb :"groups/new"
+	end
+
+	post '/groups' do
+		group = Group.new(params)
+		if group.save
+			redirect "/groups/#{group.id}"
+		else
+			flash.next[:error] = "New prayer groups require a name"
+			redirect '/groups/new'
+		end
+	end
+
+	get '/groups/:id' do
+		@group = Group.find(params[:id])
+		# binding.pry
+		if @group.private == false
+			erb :"groups/show"
+		elsif @group.private == true && @group.users.any?{|user| user.id == current_user.id}
+			erb :"groups/show"
+		else
+			flash.next[:greeting] = "You do not have access to view that group"
+			redirect '/groups'
+		end
+	end
+
+end
