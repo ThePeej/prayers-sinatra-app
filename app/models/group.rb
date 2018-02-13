74aa43492db1a00cf071b9de7940f1ed9af216a9
PJ Frias <PeejFrias@Gmail.com>
2018-02-12 23:43:06 -0500

    fix Group assoc order to allow Group#prayers

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,10 @@ class Group < ActiveRecord::Base
 	validates :name, presence: true
 	validates :leader, presence: true
 
-	has_many :prayers, through: :users
-
 	has_many :user_groups
 	has_many :members, through: :user_groups, :source => :group_member
+
+	has_many :prayers, through: :members
 
 	belongs_to :leader, :class_name => "User"
 end
