ff5e372aea1d0fa28a0f1a01d8b0b4ecabc05963
PJ Frias <PeejFrias@Gmail.com>
2018-02-12 23:45:49 -0500

    testing params from post '/prayers'

--- app/controllers/prayer_controller.rb
+++ app/controllers/prayer_controller.rb
@@ -11,6 +11,8 @@ class PrayerController < ApplicationController
 
 	post '/prayers' do
 		raise params.inspect
+		# params = {"content"=>"Prayers for me to get over my cold", "public"=>"on", "group_id"=>["2", "3"]}
+		# params = {"content"=>"TEST2", "anonymous"=>"on", "group_id"=>["1", "3"]}
 	end
 
 end
